3adafc2343be6f5ebc2963c8feeb656b9fc4f1ca
ABJ135 <1515.abubakarjaved.m2@gmail.com>
2024-10-26 13:20:56 +0500

    update index.js for id condition

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const http = require("http");
 
 const server = http.createServer((req, res)=> {
-    console.log(req.method,"server started");
+    if (req.method === "GET" && req.url === '/login') {
+
+        console.log('Login successful');
+    }
+    else if (req.method === "GET" && req.url === '/register'){
+        console.log('Registration successful');
+    }
 });
 
 const port = 8000;
 
-ServiceWorker.listen(port,()=>{
+server.listen(port,()=>{
     console.log(`listening on port ${port}`);
 });
